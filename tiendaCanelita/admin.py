631b87422fce7a9450779bd450ed86aba26a7b5c
EdwardCraft <eduardo.flores81@uabc.edu.mx>
2021-12-12 03:09:41 -0800

    app de tiendaVentas

--- tiendaCanelita/admin.py
+++ tiendaCanelita/admin.py
@@ -1,3 +1,61 @@
 from django.contrib import admin
+from tiendaCanelita.models import *
+from usuarioCanelita.models import *
+
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ("No_Producto", "Nom_Producto", "Precio", "Unidades", "Tipo", "Caracteristicas", "Imagen")
+    list_display_links = ("Nom_Producto",)
+    list_per_page = 10
+
+
+class ColaboradorAdmin(admin.ModelAdmin):
+    list_display = ("No_Colaborador", "Nom_Colaborador", "Rol", "Carrera", "Imagen_Col")
+    list_display_links = ("Nom_Colaborador",)
+    exclude = ("No_Colaborador",)
+
+
+class TipoProductoAdmin(admin.ModelAdmin):
+    list_display = ("Tipo",)
+    list_display_links = ("Tipo",)
+    list_per_page = 10
+
+
+class Registro_VentaAdmon(admin.ModelAdmin):
+    list_display = ("No_Venta", "Nom_Producto", "Unidades", "Fecha_Reg", "Total")
+    list_display_links = ("Nom_Producto",)
+    list_per_page = 10
+
+
+class VentaAdmin(admin.ModelAdmin):
+    list_display = ("No_Venta", "Nom_Producto","Precio","Unidades","Nom_Distribuidor","Total", "Fecha_Ven")
+    list_display_links = ("Nom_Producto",)
+    list_per_page = 10
+
+
+class DevolucionAdmin(admin.ModelAdmin):
+    list_display = ("No_Venta","Nom_Producto", "Causa", "Estado", "Fecha_Dev")
+    list_display_links = ("Nom_Producto",)
+    list_per_page = 10
+
+
+class DistribuidorAmdin(admin.ModelAdmin):
+    list_display = ("No_Distribuidor", "Nom_Distribuidor", "Descripcion", "Imagen_Dis")
+    list_display_links = ("Nom_Distribuidor",)
+    list_per_page = 10
+
+
+class EstadoAdmin(admin.ModelAdmin):
+    list_display = ("Estado",)
+    list_display_links = ("Estado",)
+
 
 # Register your models here.
+admin.site.register(Producto, ProductoAdmin)
+admin.site.register(Venta, VentaAdmin)
+admin.site.register(Registro_Venta, Registro_VentaAdmon)
+admin.site.register(Distribuidor, DistribuidorAmdin)
+admin.site.register(Devolución, DevolucionAdmin)
+admin.site.register(TipoProducto, TipoProductoAdmin)
+admin.site.register(Colaboradores, ColaboradorAdmin)
+admin.site.register(Estado, EstadoAdmin)
--- tiendaCanelita/views.py
+++ tiendaCanelita/views.py
@@ -1,18 +1,78 @@
-from django.shortcuts import render
-from django.shortcuts import render
+import datetime
 
+from django.shortcuts import render
+from django.shortcuts import render
+from tiendaCanelita.models import *
+import random
 def vista_home(request):
     return render(request, "home.html")
 
 def vista_prodctos(request):
-    return render(request, 'productos.html')
+    producto = Producto.objects.all()
+    return render(request, 'productos.html', {"producto": producto})
 
 def vista_distribuidores(request):
-    return render(request, 'distribuidores.html')
+    distribuidor = Distribuidor.objects.all()
+    return render(request, 'distribuidores.html', {"distribuidor": distribuidor})
 
 def vista_colaboradores(request):
-    return render(request, 'colaboradores.html')
+    colaborador = Colaboradores.objects.all()
+    return render(request, 'colaboradores.html', {"colaborador": colaborador})
 
 #Por el momento es esta ejecutando la pagina de error como view para test
 def error_404(request):
     return render(request, '404.html')
+
+def view_compra(request, id):
+    producto = Producto.objects.get(id=id)
+    venta = Venta.objects.all()
+    distribuidor = Distribuidor.objects.all()
+    return render(request, "compra.html", {'producto': producto, 'venta': venta, "distribuidor": distribuidor})
+
+def view_registroVenta(request, id):
+    producto = Producto.objects.get(id=id)
+    distribuidores = request.GET["distribuidor"]
+    distribuidores_tipo = ""
+    unidades = request.GET["unidades"]
+    total = producto.Precio * float(unidades)
+
+    if distribuidores == "1":
+        distribuidores_tipo = "DHL"
+    if distribuidores == "2":
+        distribuidores_tipo = "FedEx"
+    if distribuidores == "3":
+        distribuidores_tipo = "UPS"
+
+    distribuidor = Distribuidor.objects.get(Nom_Distribuidor=distribuidores_tipo)
+    producto = Producto.objects.get(id=id)
+
+    venta = Venta(
+        No_Venta=random.randint(0, 100000),
+        Precio=producto.Precio,
+        Unidades=float(unidades),
+        Total=total,
+        Fecha_Ven=datetime.datetime.now(),
+        Nom_Distribuidor=distribuidor,
+        Nom_Producto=producto,
+    )
+    print("venta Nom_Distribuidor: ",venta.Nom_Distribuidor)
+    venta.save()
+
+    no_venta = Venta.objects.get(No_Venta=venta.No_Venta)
+    print(no_venta.No_Venta)
+    registro = Registro_Venta(
+        No_Venta= no_venta,
+        Nom_Producto= producto.Nom_Producto,
+        Unidades= venta.Unidades,
+        Fecha_Reg= venta.Fecha_Ven,
+        Total= venta.Total
+    )
+    print("registro No_Venta: ", registro.No_Venta)
+    registro.save()
+
+    u = producto.Unidades - venta.Unidades
+    producto.Unidades = u
+    print("unidades finales: ", producto.Unidades, "Venta unidades: ", venta.Unidades)
+    producto.save()
+
+    return render(request, "registroVenta.html",{'registro':registro})
